5ed10b88291f98b2dad038c490e0eeac83beac1f
Braco <isovicramiz07@gmail.com>
2023-06-17 12:08:31 -0400

    Migrated Edcuation to functional

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,63 +6,43 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable operator-linebreak */
 /* eslint-disable react/no-array-index-key */
-import React, { Component } from "react";
-import uniqid from "uniqid";
+import React, { useState, useEffect } from "react";
+import uniqId from "uniqid";
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
+function Education() {
+  const [schools, setSchools] = useState([]);
+  const [newSchool, setNewSchool] = useState({
+    schoolName: "",
+    dateStarted: "",
+    dateEnded: "",
+    subject: "",
+    degree: "",
+    id: 0,
+  });
+  const [submittedData, setSubmittedData] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState(0);
 
-    this.state = {
-      schools: [],
-      newSchool: {
-        schoolName: "",
-        dateStarted: "",
-        dateEnded: "",
-        subject: "",
-        degree: "",
-      },
-      id: 0,
-      submittedData: [],
-      formSubmitted: false,
-      editMode: false,
-      editedSchoolIndex: null,
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-    this.switchEditState = this.switchEditState.bind(this);
-  }
-
-  handleChange(e) {
+  function handleChange(e) {
     const { name, value } = e.target;
-    const { editMode } = this.state;
-    this.setState((prevState) => ({
-      newSchool: {
-        ...prevState.newSchool,
-        [name]: value,
-      },
+    setNewSchool((prevState) => ({
+      ...prevState,
+      [name]: value,
     }));
   }
 
-  switchEditState() {
-    const { editMode } = this.state;
+  function switchEditState() {
     if (!editMode) {
-      this.setState({
-        editMode: true,
-      });
+      setEditMode(true);
     }
     if (editMode) {
-      this.setState({
-        editMode: false,
-      });
+      setEditMode(false);
     }
   }
 
-  handleEdit(e) {
+  function handleEdit(e) {
     e.preventDefault();
-    const { editMode, newSchool, schools, id } = this.state;
     const { name, value } = e.target;
     if (!editMode) {
       return;
@@ -76,141 +56,125 @@ class Education extends Component {
       }
       return school;
     });
-    this.setState({
-      schools: updatedSchools,
-      submittedData: updatedSchools,
-    });
-    console.log(this.state);
+    setSchools([...updatedSchools]);
+    setSubmittedData([...updatedSchools]);
   }
 
-  onSubmit(e) {
+  function onSubmit(e) {
     e.preventDefault();
 
-    const { newSchool, id } = this.state;
-
-    const schoolId = uniqid();
+    const schoolId = setId(uniqId);
 
     const schoolWithId = {
       ...newSchool,
       id: schoolId,
     };
 
-    this.setState((prevState) => ({
-      schools: [...prevState.schools, schoolWithId],
-      submittedData: [...prevState.schools, schoolWithId],
-      formSubmitted: true,
-      newSchool: {
-        schoolName: "",
-        dateStarted: "",
-        dateEnded: "",
-        subject: "",
-        degree: "",
-      },
-    }));
+    setSchools((prevState) => [...prevState, schoolWithId]);
+    setSubmittedData((prevState) => [...prevState, schoolWithId]);
+    setFormSubmitted(true);
+    setNewSchool({
+      schoolName: "",
+      dateStarted: "",
+      dateEnded: "",
+      subject: "",
+      degree: "",
+      id: 0,
+    });
   }
 
-  render() {
-    const {
-      schools,
-      formSubmitted,
-      schoolsKey,
-      newSchool,
-      submittedData,
-      editMode,
-    } = this.state;
-    const showResults = schools.length > 0;
-    const count = 0;
-    return (
-      <div className="edu-con">
-        <div className="education-form-div">
-          <form className="education-form" onSubmit={this.onSubmit}>
-            <label htmlFor="nameInput">SchoolName</label>
-            <input
-              onChange={this.handleChange}
-              value={newSchool.schoolName}
-              name="schoolName"
-              id="nameInput"
-            />
+  const showResults = schools.length > 0;
+  const count = 0;
+  return (
+    <div className="edu-con">
+      <div className="education-form-div">
+        <form className="education-form" onSubmit={onSubmit}>
+          <label htmlFor="nameInput">SchoolName</label>
+          <input
+            onChange={handleChange}
+            value={newSchool.schoolName}
+            name="schoolName"
+            id="nameInput"
+          />
 
-            <label htmlFor="dateStartedInput">Date Started</label>
-            <input
-              type="date"
-              onChange={this.handleChange}
-              name="dateStarted"
-              value={newSchool.dateStarted}
-              id="dateStartedInput"
-            />
+          <label htmlFor="dateStartedInput">Date Started</label>
+          <input
+            type="date"
+            onChange={handleChange}
+            name="dateStarted"
+            value={newSchool.dateStarted}
+            id="dateStartedInput"
+          />
 
-            <label htmlFor="dateEnded">Date Ended</label>
-            <input
-              type="date"
-              onChange={this.handleChange}
-              name="dateEnded"
-              value={newSchool.dateEnded}
-              id="dateEnded"
-            />
+          <label htmlFor="dateEnded">Date Ended</label>
+          <input
+            type="date"
+            onChange={handleChange}
+            name="dateEnded"
+            value={newSchool.dateEnded}
+            id="dateEnded"
+          />
 
-            <label htmlFor="subjectInput">Subject</label>
-            <input
-              type="text"
-              onChange={this.handleChange}
-              name="subject"
-              value={newSchool.subject}
-              id="subjectInput"
-            />
+          <label htmlFor="subjectInput">Subject</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="subject"
+            value={newSchool.subject}
+            id="subjectInput"
+          />
 
-            <label htmlFor="degreeInput">Degree</label>
-            <input
-              type="text"
-              name="degree"
-              onChange={this.handleChange}
-              value={newSchool.degree}
-              id="degreeInput"
-            />
+          <label htmlFor="degreeInput">Degree</label>
+          <input
+            type="text"
+            name="degree"
+            onChange={handleChange}
+            value={newSchool.degree}
+            id="degreeInput"
+          />
 
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-        <div className="edu-ul-div">
-          {formSubmitted &&
-            submittedData.map((school) => (
-              <ul key={count + 1} className="edu-results-ul">
-                {Object.keys(school).map((key) =>
-                  key !== "id" && school[key] ? (
-                    <li key={key} className="edu-results-li">
-                      {" "}
-                      {editMode ? (
-                        key === "dateStarted" || key === "dateEnded" ? (
-                          <input
-                            value={school[key]}
-                            name={key}
-                            data-id={school.id}
-                            onChange={this.handleEdit}
-                            type="date"
-                          />
-                        ) : (
-                          <input
-                            value={school[key]}
-                            name={key}
-                            data-id={school.id}
-                            onChange={this.handleEdit}
-                          />
-                        )
-                      ) : (
-                        school[key]
-                      )}
-                    </li>
-                  ) : null
-                )}
-                <button type="button" onClick={this.switchEditState}>
-                  Edit
-                </button>
-              </ul>
-            ))}
-        </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    );
-  }
+      <div className="edu-ul-div">
+        {formSubmitted &&
+          submittedData.map((school) => (
+            <ul key={school.id} className="edu-results-ul">
+              {Object.keys(school).map((key) =>
+                key !== "id" && school[key] ? (
+                  <li key={key} className="edu-results-li">
+                    {" "}
+                    {editMode ? (
+                      key === "dateStarted" || key === "dateEnded" ? (
+                        <input
+                          value={school[key]}
+                          name={key}
+                          data-id={school.id}
+                          onChange={handleEdit}
+                          type="date"
+                        />
+                      ) : (
+                        <input
+                          value={school[key]}
+                          name={key}
+                          data-id={school.id}
+                          onChange={handleEdit}
+                        />
+                      )
+                    ) : (
+                      school[key]
+                    )}
+                  </li>
+                ) : null
+              )}
+              <button type="button" onClick={switchEditState}>
+                Edit
+              </button>
+            </ul>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Education;
